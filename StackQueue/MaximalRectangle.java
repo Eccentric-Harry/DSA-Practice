b3dd357736956056423fed7f061930679b222bac
Eccentric Harry <harinathrao13@gmail.com>
2024-10-11 18:44:49 +0530

    Stack and Queue

--- StackQueue/MaximalRectangle.java
+++ StackQueue/MaximalRectangle.java
@@ -19,16 +19,55 @@ public class MaximalRectangle{
         }
         return ps;
     }
-    public static void main(String[] args){
-        char[][] matrix = {
-                            {'1', '0', '1', '0', '1'},
-                            {'1', '0', '1', '1', '1'},
-                            {'1', '1', '1', '1', '1'},
-                            {'1', '0', '0', '1', '0'}
-                        };
+    public static int maximalRectangle(char[][] matrix) {
         int[][] ps = prefixSum(matrix);
-        for(int i = 0; i < ps.length; i++){
-            System.out.println(Arrays.toString(ps[i]));
+        int max = 0;
+        for(int[] row : ps){
+            max = Math.max(max,largestRectangleArea(row));
         }
+        return max;
     }
+    public int largestRectangleArea(int[] heights) {
+        int[] nsi = nse(heights); 
+        int[] psi = pse(heights); 
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int largest = (nsi[i] - psi[i] - 1) * heights[i]; 
+            max = Math.max(largest, max);
+        }
+        
+        return max;
+    }
+
+    public int[] nse(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] nsi = new int[arr.length];
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            nsi[i] = stack.isEmpty() ? arr.length : stack.peek();  
+            stack.push(i);  
+        }
+        
+        return nsi;
+    }
+
+
+    public int[] pse(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] psi = new int[arr.length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            psi[i] = stack.isEmpty() ? -1 : stack.peek(); 
+            stack.push(i);  
+        }
+        
+        return psi;
+    }
+    
 }
