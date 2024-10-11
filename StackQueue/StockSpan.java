8fbdff6cfafa9ad3afa2619609328ce23e071fb7
Eccentric Harry <harinathrao13@gmail.com>
2024-10-11 18:43:50 +0530

    StockSpan

---
+++ StackQueue/StockSpan.java
@@ -0,0 +1,31 @@
+class StockSpanner {
+    private static class Pair{
+        int x;
+        int span;
+
+        private Pair(int x, int span){
+            this.x = x;
+            this.span = span;
+        }
+    }
+    Stack<Pair> stack; 
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+        while(!stack.isEmpty() && stack.peek().x <= price){
+            Pair p = stack.pop();
+            span = span + p.span;
+        }
+        stack.push(new Pair(price,span));
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
