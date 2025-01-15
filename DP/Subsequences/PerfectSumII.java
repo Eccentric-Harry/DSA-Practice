570f08f30d1a77ca19187919ffdb2ae5b1ae6bfb
Eccentric Harry <harinathrao13@gmail.com>
2025-01-15 12:18:01 +0530

    corrected Perfect sum approach

--- DP/Subsequences/PerfectSumII.java
+++ DP/Subsequences/PerfectSumII.java
@@ -2,28 +2,26 @@ import java.util.*;
 
 public class PerfectSumII {
      static int findWaysUtil(int ind, int target, int[] arr, int[][] dp) {
-        if (target == 0)
-            return 1;
+        
 
-        if (ind == 0)
-            return arr[0] == target ? 1 : 0;
+        if (ind == 0){
+            if (target == 0 && arr[0]==0) return 2;
+            if(target==0 || target == arr[0]) return 1;
+            return 0;
+        }
 
         if (dp[ind][target] != -1)
             return dp[ind][target];
 
-        // Calculate the number of ways when the current element is not taken
         int notTaken = findWaysUtil(ind - 1, target, arr, dp);
 
-        // Calculate the number of ways when the current element is taken
         int taken = 0;
         if (arr[ind] <= target)
             taken = findWaysUtil(ind - 1, target - arr[ind], arr, dp);
 
-        // Store and return the result for the current state
         return dp[ind][target] = notTaken + taken;
     }
 
-    // Main function to find the number of ways to form subsets with a target sum
     static int perfectSum(int[] num, int k) {
         int n = num.length;
         int dp[][] = new int[n][k + 1];
@@ -32,7 +30,24 @@ public class PerfectSumII {
             Arrays.fill(row, -1);
 
         return findWaysUtil(n - 1, k, num, dp);
+        // int ans = findWaysUtil(n - 1, k, num, dp);
+        // int zeros = countZeros(num);
+        // if(zeros!=0){
+        //     ans =  ans* (int) Math.pow(2,zeros);
+        // }
+        // return ans;
+
     }
+    public static int countZeros(int[] nums){
+        int count = 0;
+        for(int num: nums){
+            if(num == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {5, 2, 3, 10, 6, 8};
